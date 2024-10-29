3034bc933d9d1148a2f2f4b5424a94d1e9d956e1
zezo227555 <zidan4773@gmail.com>
2024-10-29 11:58:03 +0200

    last update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,32 +19,61 @@ class DatabaseSeeder extends Seeder
         User::create([
             'name' => 'admin',
             'email' => 'admin@gmail.com',
-            'password' => Hash::make('zidan227'),
+            'password' => Hash::make('admin'),
             'role' => 'مسؤول نظام',
         ]);
 
         User::create([
             'name' => 'teacher',
             'email' => 'teacher@gmail.com',
-            'password' => Hash::make('zidan227'),
+            'password' => Hash::make('teacher'),
             'role' => 'عضو هيئة تدريس',
         ]);
 
         User::create([
             'name' => 'student',
             'email' => 'student@gmail.com',
-            'password' => Hash::make('zidan227'),
+            'password' => Hash::make('student'),
             'role' => 'طالب',
         ]);
 
-        for ($i = 1; $i < 100; $i ++) {
+        for ($i = 1; $i < 4; $i ++) {
+            Room::create([
+                'name' => "CS Room $i",
+                'description' => 'normal room',
+                'capacity' => 25,
+                'available' => false,
+                'type' => 'معمل حاسوب',
+                'image' => 'room.jpg',
+            ]);
+
+            Room::create([
+                'name' => "MATH Room $i",
+                'description' => 'normal room',
+                'capacity' => 25,
+                'available' => false,
+                'type' => 'فصل',
+                'image' => 'room.jpg',
+            ]);
+
+            Room::create([
+                'name' => "BIG Room $i",
+                'description' => 'normal room',
+                'capacity' => 25,
+                'available' => false,
+                'type' => 'مدرج',
+                'image' => 'room.jpg',
+            ]);
+        }
+
+        for ($i = 1; $i < 15; $i ++) {
             Room::create([
                 'name' => "CS Room $i",
                 'description' => 'normal room',
                 'capacity' => 25,
                 'available' => true,
                 'type' => 'معمل حاسوب',
-                'image' => '1727779007.png',
+                'image' => 'room.jpg',
             ]);
 
             Room::create([
@@ -53,7 +82,7 @@ class DatabaseSeeder extends Seeder
                 'capacity' => 25,
                 'available' => true,
                 'type' => 'فصل',
-                'image' => '1727779007.png',
+                'image' => 'room.jpg',
             ]);
 
             Room::create([
@@ -62,7 +91,7 @@ class DatabaseSeeder extends Seeder
                 'capacity' => 25,
                 'available' => true,
                 'type' => 'مدرج',
-                'image' => '1727779007.png',
+                'image' => 'room.jpg',
             ]);
         }
     }
